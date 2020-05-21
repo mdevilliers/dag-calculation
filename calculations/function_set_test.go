0ce531dae30905e117af795e06c05f50db90c689
Mark deVilliers <markdevilliers@gmail.com>
2020-05-21 17:18:41 +0100

    clean up test

--- calculations/function_set_test.go
+++ calculations/function_set_test.go
@@ -1,11 +1,8 @@
 package calculations
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform/dag"
-	"github.com/hashicorp/terraform/tfdiags"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,23 +18,14 @@ func Test_FunctionSet_Build(t *testing.T) {
 	}, "one")
 	fs.RegisterIteratorFn("three", func(r runtime, rows []*DataRow) error {
 		return nil
-	})
-	/*	fs.RegisterIteratorFn("five", func(r runtime, rows []*DataRow) error {
-			return nil
-		}, "three")
-	*/
+	}, "one")
+	fs.RegisterIteratorFn("five", func(r runtime, rows []*DataRow) error {
+		return nil
+	}, "three")
+
 	err := fs.Build()
 	require.Nil(t, err)
 
-	fmt.Println(string(fs.graph.Dot(&dag.DotOpts{Verbose: true, DrawCycles: true})))
-	root, err := fs.graph.Root()
+	err = fs.Collapse(nil)
 	require.Nil(t, err)
-
-	fmt.Println(root)
-
-	fs.graph.Walk(func(v dag.Vertex) tfdiags.Diagnostics {
-		fmt.Println(v)
-		return nil
-	})
-
 }
